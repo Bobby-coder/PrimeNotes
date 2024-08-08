52b6b542d156625fe0f73b767e52c5bdd8b7a1be
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 00:27:13 +0530

    All components imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
+
 function App() {
   return (
-    <>
-      <p className="text-xl text-center">Prime Notes</p>
-    </>
+    <div className="p-2 sm:p-8 md:p-12 lg:p-16">
+      <div className="max-w-6xl mx-auto">
+        <Header />
+        <TodoList />
+      </div>
+    </div>
   );
 }
 
