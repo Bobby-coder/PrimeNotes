b822f7bc5130afef46d911f5682a47846bb94219
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 00:20:07 +0530

    Gradients data added

---
+++ src/utils/gradients.js
@@ -0,0 +1,15 @@
+export const gradients = [
+  "from-red-600 to-yellow-500",
+  "from-indigo-600 to-blue-700",
+  "from-green-600 to-lime-500",
+  "from-orange-600 to-pink-500",
+  "from-purple-600 to-indigo-500",
+  "from-blue-600 to-teal-500",
+  "from-yellow-600 to-green-500",
+  "from-pink-600 to-purple-500",
+  "from-teal-600 to-cyan-500",
+  "from-orange-600 to-red-500",
+  "from-lime-600 to-green-500",
+  "from-cyan-600 to-blue-500",
+  "from-gray-600 to-blue-700",
+];
