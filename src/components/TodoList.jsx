c8f3afca1749ed716f0d85fe9f32bb7db33c16fb
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 14:35:41 +0530

    TodoList and its setter function passed down to TodoItem component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import ToDoItem from "./ToDoItem";
 
-function ToDoList({ todos }) {
+function ToDoList({ todos, setTodoList }) {
   return (
     <div className="relative w-full overflow-auto">
       <div className="w-full flex gap-3 flex-wrap justify-center py-6 bg-white rounded-lg shadow-lg">
@@ -11,6 +11,8 @@ function ToDoList({ todos }) {
             todo={todo}
             date={date}
             completed={completed}
+            setTodoList={setTodoList}
+            allTodos={todos}
           />
         ))}
       </div>
