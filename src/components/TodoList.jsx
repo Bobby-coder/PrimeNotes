38909794d00649d6eb4c693d68c3919a932c45be
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 19:13:57 +0530

    some props recieved and passed down to the todo item component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,6 @@
 import ToDoItem from "./ToDoItem";
 
-function ToDoList({ todos, setTodoList }) {
+function ToDoList({ todos, setTodoList, setEditingId, setTodoString }) {
   return (
     <div className="relative w-full overflow-auto">
       <div className="w-full flex gap-3 flex-wrap justify-center py-6 bg-white rounded-lg shadow-lg">
@@ -13,6 +13,8 @@ function ToDoList({ todos, setTodoList }) {
             completed={completed}
             setTodoList={setTodoList}
             allTodos={todos}
+            setTodoString={setTodoString}
+            setEditingId={setEditingId}
           />
         ))}
       </div>
