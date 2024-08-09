ac41458b7607cec99985323a432a6d32a5794de3
Bobby-coder <bobbysadhwani612@gmail.com>
2024-08-09 19:52:41 +0530

    Added a placeholder text when no todo is present

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,6 +4,11 @@ function ToDoList({ todos, setTodoList, setEditingId, setTodoString }) {
   return (
     <div className="relative w-full overflow-auto">
       <div className="w-full flex gap-3 flex-wrap justify-center py-6 bg-white rounded-lg shadow-lg">
+        {!todos.length && (
+          <div className="text-gray-500 text-center py-6">
+            No tasks available. Add some tasks to get started!
+          </div>
+        )}
         {todos.map(({ id, todo, date, completed }) => (
           <ToDoItem
             key={crypto.randomUUID()}
